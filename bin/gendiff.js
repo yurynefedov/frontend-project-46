92115b1391984b2e9b6ac342eaf014876b8fae43
yurynefedov <yury.a.nefedov@gmail.com>
2022-11-29 18:56:30 +0400

    feat: add difference generator function working with json files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -7,6 +8,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,75 @@
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+
+const getPathToFile = (filepath) => path.resolve(process.cwd(), filepath).trim();
+const getFileData = (filepath) => JSON.parse(fs.readFileSync(getPathToFile(filepath), 'utf-8'));
+
+const getDiffData = (filepath1, filepath2) => {
+  const data1 = getFileData(filepath1);
+  const data2 = getFileData(filepath2);
+
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+
+  const keys = _.sortBy(_.union(keys1, keys2));
+
+  const diffData = keys.map((key) => {
+    const initialValue = data1[key];
+    const amendedValue = data2[key];
+
+    switch (true) {
+      case (!_.has(data1, key) && _.has(data2, key)):
+        return { key, value: data2[key], status: 'added' };
+      case (_.has(data1, key) && !_.has(data2, key)):
+        return { key, value: data1[key], status: 'deleted' };
+      case (_.has(data1, key) && _.has(data2, key)):
+        if (initialValue === amendedValue) {
+          return { key, value: initialValue, status: 'unmodified' };
+        } return {
+          key, initialValue, amendedValue, status: 'modified',
+        };
+      default:
+        throw new Error(`Unknown state of property '${key}'`);
+    }
+  });
+
+  return diffData;
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const diffData = getDiffData(filepath1, filepath2);
+
+  const result = [];
+  diffData.forEach((data) => {
+    const {
+      key,
+      value,
+      initialValue,
+      amendedValue,
+      status,
+    } = data;
+
+    switch (status) {
+      case 'added':
+        result.push(`  + ${key}: ${value}`);
+        break;
+      case 'deleted':
+        result.push(`  - ${key}: ${value}`);
+        break;
+      case 'unmodified':
+        result.push(`    ${key}: ${value}`);
+        break;
+      case 'modified':
+        result.push(`  - ${key}: ${initialValue}`);
+        result.push(`  + ${key}: ${amendedValue}`);
+        break;
+      default:
+        throw new Error(`Unknown state of property '${key}'`);
+    }
+  });
+
+  return `{\n${result.join('\n')}\n}`;
+};
+
+export default genDiff;
