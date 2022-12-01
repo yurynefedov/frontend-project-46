247bcf472c6454d3878e849f9f0c77b2e97cfd20
yurynefedov <yury.a.nefedov@gmail.com>
2022-12-01 14:45:49 +0400

    feat: add tests for plain yaml files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,17 +1,25 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
-import expectedData from '../__fixtures__/test-data/plain-json-expected.js';
+import expectedDataPlain from '../__fixtures__/test-data/plain-expected.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getPathToFixturesFiles = (filename) => path.join(__dirname, '..', '__fixtures__', 'files', filename);
 
-test('Get difference of two json files', () => {
-  const file1 = getPathToFixturesFiles('file1.json');
-  const file2 = getPathToFixturesFiles('file2.json');
-  const receivedData = genDiff(file1, file2);
+const jsonFile1 = getPathToFixturesFiles('file1.json');
+const jsonFile2 = getPathToFixturesFiles('file2.json');
 
-  expect(receivedData).toEqual(expectedData);
+const yamlFile1 = getPathToFixturesFiles('file1.yaml');
+const yamlFile2 = getPathToFixturesFiles('file2.yaml');
+
+test('Get difference of two plain json files', () => {
+  const receivedData = genDiff(jsonFile1, jsonFile2);
+  expect(receivedData).toEqual(expectedDataPlain);
+});
+
+test('Get difference of two plain yaml files', () => {
+  const receivedData = genDiff(yamlFile1, yamlFile2);
+  expect(receivedData).toEqual(expectedDataPlain);
 });
