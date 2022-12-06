1bc684e5bb1995d54ec7f7752778ac681b4de0c4
yurynefedov <yury.a.nefedov@gmail.com>
2022-12-06 21:14:42 +0400

    feat: add logic for output rendering in plain format

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,3 +1,42 @@
-const renderPlain = () => {};
+import _ from 'lodash';
+
+const normalizeData = (value) => {
+  const flatData = _.isString(value) ? `'${value}'` : value;
+  return _.isObject(value) ? '[complex value]' : flatData;
+};
+
+const renderPlain = (diffTree) => {
+  const iter = (node, path) => {
+    const lines = node.flatMap((element) => {
+      const {
+        key,
+        value,
+        initialValue,
+        amendedValue,
+        children,
+        status,
+      } = element;
+
+      const currentPath = path === '' ? `${key}` : `${path}.${key}`;
+
+      switch (status) {
+        case 'nested':
+          return iter(children, currentPath);
+        case 'modified':
+          return `Property '${currentPath}' was updated. From ${normalizeData(initialValue)} to ${normalizeData(amendedValue)}`;
+        case 'added':
+          return `Property '${currentPath}' was added with value: ${normalizeData(value)}`;
+        case 'deleted':
+          return `Property '${currentPath}' was removed`;
+        case 'unmodified':
+          return [];
+        default:
+          throw new Error(`Invalid status '${status}' of property '${key}'`);
+      }
+    });
+    return lines.join('\n');
+  };
+  return iter(diffTree, '');
+};
 
 export default renderPlain;
