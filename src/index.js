73a596e899f8ac50da8958407cce18395f595588
yurynefedov <yury.a.nefedov@gmail.com>
2022-12-12 16:24:04 +0400

    refactor: refactoring of parser func

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import fs from 'fs';
+import path from 'path';
 import parseFileData from './parsers.js';
 import buildDiffTree from './diff-tree.js';
 import render from './formatters/index.js';
 
+const getPathToFile = (filepath) => path.resolve(process.cwd(), filepath).trim();
+const readFileData = (filepath) => fs.readFileSync(getPathToFile(filepath), 'utf-8');
+const getExtention = (filepath) => path.extname(filepath).toLowerCase();
+
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const data1 = parseFileData(filepath1);
-  const data2 = parseFileData(filepath2);
+  const data1 = parseFileData(readFileData(filepath1), getExtention(filepath1));
+  const data2 = parseFileData(readFileData(filepath2), getExtention(filepath2));
   const diffTree = buildDiffTree(data1, data2);
 
   return render(diffTree, format);
--- src/parsers.js
+++ src/parsers.js
@@ -1,18 +1,12 @@
 import yaml from 'js-yaml';
-import fs from 'fs';
-import path from 'path';
 
-const getPathToFile = (filepath) => path.resolve(process.cwd(), filepath).trim();
-const readFileData = (filepath) => fs.readFileSync(getPathToFile(filepath), 'utf-8');
-
-const parseFileData = (filepath) => {
-  const extention = path.extname(filepath).toLowerCase();
+const parseFileData = (filedata, extention) => {
   switch (extention) {
     case '.json':
-      return JSON.parse(readFileData(filepath));
+      return JSON.parse(filedata);
     case '.yml':
     case '.yaml':
-      return yaml.load(readFileData(filepath));
+      return yaml.load(filedata);
     default:
       throw new Error(`Sorry, the '${extention}' extention is not supported`);
   }
