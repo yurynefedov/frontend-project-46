84717195c57eccee3b9670bc3ae2944c68988ce8
yurynefedov <yury.a.nefedov@gmail.com>
2022-12-15 13:32:33 +0400

    refactor: adjust test data and test cases

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,23 +9,16 @@ const __dirname = path.dirname(__filename);
 const getPathToFixturesFiles = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFileData = (filename) => fs.readFileSync(getPathToFixturesFiles(filename), 'utf-8');
 
-const testData = [
-  ['json', 'stylish'],
-  ['json', 'plain'],
-  ['json', 'json'],
-  ['yaml', 'stylish'],
-  ['yaml', 'plain'],
-  ['yaml', 'json'],
-];
+const testFormats = ['json', 'yaml'];
 
-test.each(testData)('Get difference of two %s files, %s format', (extension, outputFormat) => {
-  const path1 = getPathToFixturesFiles(`before.${extension}`);
-  const path2 = getPathToFixturesFiles(`after.${extension}`);
+test.each(testFormats)('Get difference of two %s files', (format) => {
+  const path1 = getPathToFixturesFiles(`before.${format}`);
+  const path2 = getPathToFixturesFiles(`after.${format}`);
 
-  // Checking default output format (function call without output format as an argument)
   expect(genDiff(path1, path2)).toBe(readFileData('stylish.txt'));
+  expect(genDiff(path1, path2, 'stylish')).toBe(readFileData('stylish.txt'));
+  expect(genDiff(path1, path2, 'plain')).toBe(readFileData('plain.txt'));
+  expect(genDiff(path1, path2, 'json')).toBe(readFileData('json.txt'));
   // Checking generation of correct json file at reverse conversion
   expect(() => JSON.parse(genDiff(path1, path2, 'json'))).not.toThrow();
-  // Checking available output formats
-  expect(genDiff(path1, path2, outputFormat)).toBe(readFileData(`${outputFormat}.txt`));
 });
